d7372b590741703b8378717ea532939919c8af3a
olle <olle.axelsson@elev.ga.ntig.se>
2024-09-17 11:04:32 +0200

    youtube video

--- server.js
+++ server.js
@@ -17,32 +17,67 @@ app.use(express.static("public"))
 
 
 app.get("/", (request, response) => {
-response.render("index.njk", {
+    console.log(request.query)
+    const name = request.query.name
+    response.render("index.njk", {
 
-    message: "Hello world!",
-    title: "Hem",
-    items: ["A", "B", "C", "D"],
-})
+        message: `Hallåj ${name}`,
+        title: "Hem",
+        items: ["A", "B", "C", "D"],
+    })
 
-app.get("/om", (request, response) => {
-})
-
-})
-
-app.get("/readme", (request, response) =>{
-console.log(Request)
-
-response.send("Bara en response")
-
+    app.get("/om", (request, response) => {
+    })
 
 })
 
+app.get("/readme", (request, response) => {
+    console.log(Request)
+
+    response.send("Bara en response")
+
+
+})
+
+
+
+
+app.get(`/watch`, (request, response) => {
+    const movieID = request.query.v
+    console.log(movieID)
+
+    const movies = {
+        "ID": {
+            title: "Ahmeds pizzeria",
+            year: "2019",
+        }
+    }
+    const movie = movies[movieID]
+    console.log(movie)
+
+    response.render("watch.njk", {
+        title: "watch",
+        movie: movie,
+    })
+
+
+    //response.json(movie)
+
+})
+
+app.get(`/ytub`, (request, response) => {
+
+    const ID = request.query.v
+    console.log(ID)
+    response.render(`ytub.njk`, {
+        title: "youtube",
+        ID: ID,
+
+    })
+})
 
 app.listen(3000, () => {
 
     console.log('Server is running on http://localhost:3000')
 
 })
-
-
-
