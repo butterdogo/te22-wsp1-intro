faa458d98330d7b74a27cfaea3f82074e39fba30
olle <olle.axelsson@elev.ga.ntig.se>
2024-09-24 11:12:13 +0200

    search

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express, { response } from "express"
 import nunjucks from "nunjucks"
 import morgan from "morgan"
 import indexRouter from "./routes/index.js"
+import searchRouter from "./routes/search.js"
 
 const app = express()
 
@@ -13,11 +14,12 @@ nunjucks.configure("views", {
 
 })
 
-app.use("/", indexRouter)
+
 
 app.use(express.static("public"))
 app.use(morgan("dev"))
-
+app.use("/search", searchRouter)
+app.use("/", indexRouter)
 
 app.use((req, res) => {
 
